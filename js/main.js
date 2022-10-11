51ed3091a0d3082b0f1bace7958b6acfa1d04eee
masha8709 <masha.8709@mail.ru>
2022-10-11 16:49:53 +0300

    Обновления от Кекса

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //Функцию взяла с сайта https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  min = Math.ceil(Math.min(min, max));
+  max = Math.floor(Math.max(min, max));
   if (typeof min !== 'number' || typeof max !== 'number' || min < 0 || max < 0) {
     return NaN;
   }
