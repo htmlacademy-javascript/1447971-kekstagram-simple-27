c5ae62b43780a72cfff67dd26b61ec5ef8d5fa9c
masha8709 <masha.8709@mail.ru>
2022-10-11 18:27:50 +0300

    Добавила функции для создания описания фото

--- js/main.js
+++ js/main.js
@@ -23,3 +23,47 @@ function getLength(description, length) {
 }
 
 getLength('Текстовый комментарий', 140);
+
+
+//массив случайной длины из значений, значения не повторяются
+function getListElements(counter) {
+  const newArray = [];
+  for (let i = 1; i <= counter; i++) {
+    const newElem = i;
+    newArray.push(newElem);
+  }
+  return newArray;
+}
+
+
+//адрес картинки
+
+function getUrl(array) {
+  let url = '';
+  const rand = Math.floor(Math.random() * array.length);
+  url = `photos/${array[rand]}.jpg`;
+  return url;
+}
+
+//даёт одно значение из массива. НЕ ЗНАЮ, КАК СДЕЛАТЬ, ЧТОБЫ НЕ ПОВТОРЯЛОСЬ ЗНАЧЕНИЕ
+const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+
+const DESCRIPTION_COUNT = 25;
+
+const ID_ARRAY = getListElements(25);
+
+const CREATE_PHOTO_DESCRIPTION = () => ({
+  id: getRandomArrayElement(ID_ARRAY),
+  url: getUrl(ID_ARRAY),
+  description: 'Красивая фотография',
+  likes: getRandomIntInclusive(15, 200),
+  comments: getRandomIntInclusive(0, 200)
+});
+
+CREATE_PHOTO_DESCRIPTION();
+
+//Сохраняла код ниже (Array.from...) в константу PHOTO_DESCRIPTION, но ESlint всё время ругается,
+//что она не используется, но если её вывести в консоль, снова начинает ругаться
+Array.from({
+  length: DESCRIPTION_COUNT
+}, CREATE_PHOTO_DESCRIPTION);
