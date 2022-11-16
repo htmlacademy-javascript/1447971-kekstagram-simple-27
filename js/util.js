da7e97474539522ad8cf7508c0a3b49916021ef2
masha8709 <masha.8709@mail.ru>
2022-11-16 14:01:27 +0300

    поменяла функции на стрелочные

--- js/util.js
+++ js/util.js
@@ -1,4 +1,4 @@
-function getRandomIntInclusive(min, max) {
+const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(Math.min(min, max));
   max = Math.floor(Math.max(min, max));
   if (typeof min !== 'number' || typeof max !== 'number' || min < 0 || max < 0) {
@@ -11,23 +11,21 @@ function getRandomIntInclusive(min, max) {
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 getRandomIntInclusive(2, 5);
 
-function getLength(description, length) {
-  return description.length <= length;
-}
+const getLength = (description, length) => description.length <= length;
 
 getLength('Текстовый комментарий', 140);
 
-function getListElements(counter) {
+const getListElements = (counter) => {
   const newArray = [];
   for (let i = 1; i <= counter; i++) {
     const newElem = i;
     newArray.push(newElem);
   }
   return newArray;
-}
+};
 
 //даёт одно значение из массива. НЕ ЗНАЮ, КАК СДЕЛАТЬ, ЧТОБЫ НЕ ПОВТОРЯЛОСЬ ЗНАЧЕНИЕ
 const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
