974ac8cddd0f0e4b745b075ff4eb64530500cab2
masha8709 <masha.8709@mail.ru>
2022-11-25 19:05:53 +0300

    ещё раз переписала слайдер

--- js/effect-img.js
+++ js/effect-img.js
@@ -8,55 +8,79 @@ const EFFECTS = {
   'none': {
     style: 'none',
     unit: '',
-    min: 1,
-    max: 100,
-    start: 100,
-    step: 1
+    options: {
+      range: {
+        min: 1,
+        max: 100,
+      },
+      start: 100,
+      step: 1
+    }
   },
 
   'chrome': {
     style: 'grayscale',
     unit: '',
-    min: 0,
-    max: 1,
-    start: 100,
-    step: 0.1
+    options: {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      start: 1,
+      step: 0.1
+    }
   },
 
   'sepia': {
     style: 'sepia',
     unit: '',
-    min: 0,
-    max: 1,
-    start: 1,
-    step: 0.1
+    options: {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      start: 1,
+      step: 0.1
+    }
   },
 
   'marvin': {
     style: 'invert',
     unit: '%',
-    min: 0,
-    max: 100,
-    start: 100,
-    step: 1
+    options: {
+      range: {
+        min: 0,
+        max: 100,
+      },
+      start: 100,
+      step: 1
+    }
   },
 
   'phobos': {
     style: 'blur',
     unit: 'px',
-    min: 0,
-    max: 3,
-    start: 3,
-    step: 0.1
+    options: {
+      range: {
+        min: 0,
+        max: 3,
+      },
+      start: 3,
+      step: 0.1
+    }
   },
 
   'heat': {
     style: 'brightness',
     unit: '',
-    min: 1,
-    max: 3,
-    start: 3,
-    step: 0.1
+    options: {
+      range: {
+        min: 1,
+        max: 3,
+      },
+      start: 3,
+      step: 0.1
+    }
   }
 };
 
@@ -73,69 +97,52 @@ const applyEffectClass = (effectName) => {
 };
 
 const loadEffect = (effectName) => {
-  sliderEffect.classList.remove('hidden');
-  //sliderContainer.style.display = 'block';
   selectedEffect = EFFECTS[effectName];
 
-  sliderEffect.noUiSlider.updateOptions({
-    range: {
-      min: selectedEffect.min,
-      max: selectedEffect.max
-    },
-    step: selectedEffect.step,
-    start: selectedEffect.start
-  });
+  sliderEffect.noUiSlider.updateOptions(selectedEffect.options);
 
   applyEffectClass(effectName);
-
-  // if (DEFAULT_EFFECT) {
-  //   sliderEffect.classList.add('hidden');
-  //   sliderContainer.style.display = 'none';
-  // }
 };
 
 const onEffectValueUpdate = () => {
   if (selectedEffect.style === DEFAULT_EFFECT) {
     sliderContainer.classList.add('hidden');
-    //sliderContainer.style.display = 'none';
-    imagePreview.style.filter = 'none';
+
+    imagePreview.style.filter = '';
   } else {
     sliderContainer.classList.remove('hidden');
-    //sliderContainer.style.display = 'block';
+
     effectLevelValueElement.value = sliderEffect.noUiSlider.get();
     imagePreview.style.filter = `${selectedEffect.style}(${effectLevelValueElement.value}${selectedEffect.unit})`;
   }
 };
 
-// const resetEffect = () => {
-//   selectedEffect = DEFAULT_EFFECT;
-//   onEffectValueUpdate();
-// };
-
 const onTargetEffectChange = (evt) => {
   loadEffect(evt.target.value);
 };
 
-noUiSlider.create(sliderEffect, {
-  range: {
-    min: 1,
-    max: 100,
-  },
-  start: 100,
-  step: 1,
-  connect: 'lower'
-});
+const createSlider = () => {
+  noUiSlider.create(sliderEffect, {
+    range: {
+      min: 1,
+      max: 100,
+    },
+    start: 100,
+    step: 1,
+    connect: 'lower'
+  });
 
-onEffectValueUpdate();
-form.addEventListener('change', onTargetEffectChange);
-sliderEffect.noUiSlider.on('update', onEffectValueUpdate);
+  loadEffect(DEFAULT_EFFECT);
 
+  sliderEffect.noUiSlider.on('update', onEffectValueUpdate);
+  form.addEventListener('change', onTargetEffectChange);
+};
 
-// const destroySlider = () => {
-//   sliderEffect.noUiSlider.off('update');
-//   sliderEffect.noUiSlider.destroy();
+const destroySlider = () => {
+  sliderEffect.noUiSlider.off('update');
+  sliderEffect.noUiSlider.destroy();
 
-//   form.removeEventListener('change', onTargetEffectChange);
-// };
+  form.removeEventListener('change', onTargetEffectChange);
+};
 
-//export {resetEffect};
+export {createSlider, destroySlider};
